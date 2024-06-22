a3e52ca3c812996eeb80587814348ef431b4d8a8
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-06-22 07:48:10 +0530

    Add project files.

---
+++ SearchValuesExample/SearchValuesBenchMarks.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using System.Buffers;
+
+namespace SearchValuesExample;
+
+[Config(typeof(StyleConfig))]
+[HideColumns(Column.RatioSD)]
+public class SearchValuesBenchMarks
+{
+    private static readonly char[] vowelsCharArr = ['a', 'e', 'i', 'o', 'u'];
+    private static readonly SearchValues<char> vowelsSearchValues = SearchValues.Create("aeiou");
+
+    [Params("aeiouqazwsxedcrfv")]
+    public string SourecText { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public bool IsAllVowels_CharArray()
+    {
+        return SourecText.AsSpan().IndexOfAnyExcept(vowelsCharArr) == -1;
+    }
+
+    [Benchmark]
+    public bool IsAllVowels_SearchValues()
+    {
+        return SourecText.AsSpan().IndexOfAnyExcept(vowelsSearchValues) == -1;
+    }  
+}
+
+public class StyleConfig : ManualConfig
+{
+    public StyleConfig()
+    {
+        SummaryStyle = SummaryStyle.Default.WithRatioStyle(RatioStyle.Trend);
+    }
+}
